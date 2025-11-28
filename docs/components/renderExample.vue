d1875dadceffb17f86c36ce3238a8cbb8cc2a2fa
dullcat <cat_cxy@qq.com>
2025-11-28 22:50:17 +0800

    1

---
+++ docs/components/renderExample.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="relative">
+    <div class="absolute top-4 z-2 flex justify-center w-full">
+      <div class="flex items-center">
+        <div
+          class="hover:text-white hover:bg-blue-600 bg-white border-2 border-blue-200 mr-2 rounded-xl has-checked:bg-blue-600 has-checked:text-white">
+          <input type="radio" id="img" name="layer" value="img" checked class="hidden" />
+          <label for="img" class="h-10 p-2 flex items-center">影像底图</label>
+        </div>
+        <div
+          class="hover:text-white hover:bg-blue-600 bg-white border-2 border-blue-200 mr-2 rounded-xl has-checked:bg-blue-600 has-checked:text-white">
+          <input type="radio" id="vec" name="layer" value="vec" class="hidden" />
+          <label for="vec" class="h-10 p-2 flex items-center">矢量底图</label>
+        </div>
+      </div>
+    </div>
+    <div id="map" class="w-full h-50vh"></div>
+  </div>
+</template>
+
+<script setup>
+import olHelper, { td4326WMTSPreset } from '../../olHelper/olHelper';
+
+onMounted(() => {
+  let tdKey = '086d31664864bb1b890c28084f786ca8';
+  let _olHelper = new olHelper('map');
+  _olHelper.map.addLayer(td4326WMTSPreset(tdKey, 'img_c'));
+  _olHelper.map.addLayer(td4326WMTSPreset(tdKey, 'cva_c'));
+  _olHelper.map.addLayer(td4326WMTSPreset(tdKey, 'vec_c', false));
+  _olHelper.map.addLayer(td4326WMTSPreset(tdKey, 'cva_c', false));
+
+  _olHelper.setMapCenter([114.393569, 30.50846], 18);
+
+  // 获取DOM元素
+  const option1 = document.getElementById('img');
+  const option2 = document.getElementById('vec');
+
+  // 添加事件监听器
+  option1.addEventListener('change', handleRadioChange);
+  option2.addEventListener('change', handleRadioChange);
+
+  // 事件处理函数
+  function handleRadioChange(event) {
+    const selectedValue = event.target.value;
+    closeAllBaseLayer();
+    if (selectedValue === 'img') {
+      _olHelper.getLayerByName('img_c').setVisible(true);
+      _olHelper.getLayerByName('cva_c').setVisible(true);
+    } else {
+      _olHelper.getLayerByName('vec_c').setVisible(true);
+      _olHelper.getLayerByName('cva_c').setVisible(true);
+    }
+  }
+
+  // 关闭所有底图,td4326WMTSPreset函数生成的图层会自带type属性,值为baseLayer
+  function closeAllBaseLayer() {
+    let totalLayer = _olHelper.map.getLayers().getArray();
+    totalLayer
+      .filter((item) => item.get('type') === 'baseLayer')
+      .forEach((layer) => {
+        layer?.setVisible(false);
+      });
+  }
+});
+</script>
+
+<style scoped></style>
